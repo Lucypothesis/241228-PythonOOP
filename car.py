5de7ed98ceb80ee6ef6a11f5cdadbcb61a10737a
Lucy Park <lucypothesis@gmail.com>
2024-12-28 14:26:53 +0900

    Initial commit

---
+++ car.py
@@ -0,0 +1,15 @@
+class Car:
+    # dunder: double underscore
+    # 어떤 항목이 들어가야 하는지 적기
+    def __init__(self, model, year, color, for_sale):
+        self.model = model
+        self.year = year
+        self.color = color
+        self.for_sale = for_sale
+
+    # Identical for each car object
+    def drive(self):
+        print(f"You drive the {self.model}")
+
+    def stop(self):
+        print(f"You stop the {self.model}")
---
+++ main.py
@@ -0,0 +1,14 @@
+from car import Car
+
+car1 = Car("Mustang", 2024, "red", False)
+car2 = Car("Corvette", 2025, "blue", True)
+car3 = Car("Charger", 2026, "yellow", True)
+
+print(car3.model)
+print(car3.year)
+print(car3.color)
+print(car3.for_sale)
+
+car1.drive()
+car2.drive()
+car3.drive()
